137335a66798fad843e9b63e01ad11d389f4fe18
eslemalemdar <eslemalemdar98@gmail.com>
2023-10-29 23:56:48 +0100

    bug fix

--- word_counts.py
+++ word_counts.py
@@ -1,47 +1,59 @@
 import pandas as pd
-df = pd.read_csv("final_data.csv", header=0)
+from itertools import repeat
+from multiprocessing import cpu_count
+from multiprocessing import Pool
+import tqdm
 
-fix_df = pd.read_csv("fixdata_table.csv", header=0)
+current_file_name=None
 
-count=0
-current_file_name= None
+def process(input, df, fix_df):
+    index = input[0]
+    row = input[1]
 
-for index, row in df.iterrows():
-    # COUNT LOGIC
-    if current_file_name is None:
-        current_file_name = row["file_name"]
-
-    if current_file_name == row["file_name"]:
-        if row["part_of_speech_simple"] != 'PUNCT':
-            count = count + 1
-    else:
-        df.loc[df["file_name"] == current_file_name, "total_word_count"] = count
-        count = 0
-        current_file_name = None
+    count = len(df[(df["file_name"] == row['file_name']) & (df['part_of_speech_simple'] != 'PUNCT')])
+    df.at[index, 'total_word_count'] = count
 
     # RE-MENTIONED LOGIC
-    current_singular_label = row['singLabel']
-    re_mentions = df[(df['file_name'] == current_file_name) & (df['singLabel'] == current_singular_label)]
-    re_mentions_count = len(re_mentions)
-    df.at[index, 're_mentions_count'] = re_mentions_count
-    is_re_mentioned = re_mentions_count > 1
+    mentions = df[(df['file_name'] == row['file_name']) & (df['singLabel'] == row['singLabel']) & (str(df['bb']) != 'nan')]
+
+    mentions_count = len(mentions)
+    df.at[index, 're_mentions_count'] = mentions_count
+    is_re_mentioned = mentions_count > 1
     df.at[index, 'is_re_mentioned'] = is_re_mentioned
 
-    if is_re_mentioned:
-        first_remention = re_mentions[re_mentions['sentenceLevel'] == re_mentions['sentenceLevel'].min()]
-        first_remention_sentence_level = first_remention.iloc[0]['sentenceLevel']
-        df.at[index, 'first_remention_sentence_level'] = first_remention_sentence_level
+    if len(mentions) > 0:
+        first_mention = mentions[mentions['sentenceLevel'] == mentions['sentenceLevel'].min()]
+        first_mention_sentence_level = first_mention.iloc[0]['sentenceLevel']
+        df.at[index, 'first_remention_sentence_level'] = first_mention_sentence_level
+    
+        first_mention_word_index = first_mention[first_mention['word_index'] == first_mention['word_index'].min()].iloc[0]['word_index']
+    
+        df.at[index, 'first_remention_word_index'] = first_mention_word_index
 
-        first_remention_word_index = first_remention[first_remention['word_index'] == first_remention['word_index'].min()].iloc[0]['word_index']
-        df.at[index, 'first_remention_word_index'] = first_remention_word_index
+        # JOINING WITH FIXDATA LOGIC
+        fix_df.loc[fix_df['image_path'].str.contains(str(row['file_name'])) & fix_df['objectLabel'].str.contains(str(row['label'])), [
+            'mentions_count',
+            'is_re_mentioned',
+            'first_mention_sentence_level',
+            'first_mention_word_index'
+        ]] = [
+            mentions_count,
+            is_re_mentioned,
+            first_mention_sentence_level,
+            first_mention_word_index
+        ]
 
-    # JOINING WITH FIXDATA LOGIC
-    fix_df.loc[fix_df['image_path'].str.contains(str(current_file_name)) & fix_df['objectLabel'].str.contains(str(row['label'])), ['re_mentions_count','is_re_mentioned','first_remention_sentence_level','first_remention_word_index']] = [
-        re_mentions_count,
-        is_re_mentioned,
-        first_remention_sentence_level,
-        first_remention_word_index
-    ]
+def main():
+    df = pd.read_csv("final_data.csv", header=0)
+    fix_df = pd.read_csv("fixdata_table.csv", header=0)
 
-df.to_csv('with_word_count.csv', index=False)
-fix_df.to_csv('fix_data_merged.csv', index=False)
+    pool = Pool(16)
+    pool.starmap(process, tqdm.tqdm(zip(df.iterrows(), repeat(df), repeat(fix_df)), total=len(df)))
+
+    df.to_csv('with_word_count.csv', index=False)
+    fix_df.to_csv('fix_data_merged.csv', index=False)
+
+if __name__ == '__main__':
+    main()
+
+#word index -- should start from 1 not 0
